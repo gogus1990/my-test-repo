70c57cf4934ee540fcd89fe4bac5841c37055f97
Piotr Pikosz <piotr.pikoszofficial@gmail.com>
2024-02-03 18:09:09 +0100

    commit3

--- Person.java
+++ Person.java
@@ -2,5 +2,6 @@ public class Person {
     public static void main(String[] args) {
         System.out.println("ssss");
         System.out.println("dsadasdasfas");
+        System.out.println("dsad");
     }
 }
